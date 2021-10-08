0a77b57bc7922541784d4dd833020e33624851fc
rochiAmaya <rochiamaya@gmail.com>
2021-10-08 13:50:44 -0300

    navegacion entre paginas, editar y eliminar rutina primer esqueleto de timer

--- src/Routines/components/ListRoutines.js
+++ src/Routines/components/ListRoutines.js
@@ -1,18 +1,30 @@
-import React from "react";
-import {Card, Columns, Content, Heading} from "react-bulma-components";
+import React, {useState} from "react";
+import {Card, Columns, Container, Content, Heading, Image} from "react-bulma-components";
+import MyModal from "./MyModal";
+import {deleteRoutine} from "../services";
+const ListRoutines = ({routines, loadRoutines}) =>{
+    const [isModalOpenParam, setIsModalOpen] = useState(false)
+    const [routineActual, setRoutineActual] = useState(null)
 
-const ListRoutines = ({routines}) =>{
     return(
+        <Container>
           <Columns>
               {
-                  routines.map(({_id, name, description,steps,alarm,background})=> (
-                      <Columns.Column size="4" key={_id}>
+                  routines.map((routine)=> (
+                      <Columns.Column size="4" key={routine._id}>
                           <Card>
-                            <Card.Content>
+                              <Card.Image display={true}>
+                                  <Image src={"https://bulma.io/images/placeholders/1280x960.png"} size={8}></Image>
+                              </Card.Image>
+                              <Card.Content>
                                 <Content>
-                                    <Heading>{name}</Heading>
-                                    <Heading subtitle size={6}>{description}</Heading>
-
+                                    <Heading>{routine.name}</Heading>
+                                    <Heading subtitle size={6}>{routine.description}</Heading>
+                                    <Card.Footer>
+                                        <a href="#" className="card-footer-item">Ejecutar</a>
+                                        <a onClick={()=>{setIsModalOpen(true); setRoutineActual(routine)}} className="card-footer-item">Editar</a>
+                                        <a onClick={()=>{deleteRoutine({routine});loadRoutines()}} className="card-footer-item">Eliminar</a>
+                                    </Card.Footer>
                                 </Content>
                             </Card.Content>
                           </Card>
@@ -20,7 +32,9 @@ const ListRoutines = ({routines}) =>{
                   ))                  }
 
           </Columns>
-    )
+         <MyModal isModalOpen={isModalOpenParam} setIsModalOpen={setIsModalOpen} routine = {routineActual} loadRoutines={loadRoutines}></MyModal>
+        </Container>
+            )
 
 }
 export default ListRoutines
